70b179aae1d86f60d3c39447d2e175b0b3c69947
Mindaugas Liubinas <mindaugas.l@estina.lt>
2015-02-09 20:47:12 +0200

    Cleanup. Form display

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Home:index.html.twig');
+    }
+
+    /**
+     * @Route("/")
+     * @Method("POST")
+     */
+    public function generateAction()
+    {
+        // validate form & generate pizzas
+    }
+}
