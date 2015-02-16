97a9630fb3feb737d9ad3c0948506b9591bdf388
Mindaugas Liubinas <mindaugas.l@estina.lt>
2015-02-16 14:05:15 +0200

    Disable security. Load cities

--- web/js/app/home.js
+++ web/js/app/home.js
@@ -3,10 +3,44 @@ var App = App || {};
 (function($) {
 
     App.Home = {
+
+        setPlaceHtml: function(html) {
+            $('#place-ctr').html(html);
+        },
+
+        setCityHtml: function(html) {
+            $('#city-ctr').html(html);
+        },
+
         init: function() {
-            console.log('home init')
+            var _this = this;
             $('#country').on('change', function(){
 
+                var countryCode = $(this).val();
+
+                if ('' == countryCode) {
+                    _this.setCityHtml(
+                        '<select id="city" name="city" class="form-control" disabled>' +
+                        '<option value="">select</option>' +
+                        '</select>'
+                    );
+
+                } else {
+
+                    $.ajax({
+                        type: 'get',
+                        url: '/cities/' + countryCode,
+                        success: function(response) {
+                            _this.setCityHtml(response);
+                        }
+                    });
+                }
+
+                _this.setPlaceHtml(
+                    '<select id="city" name="city" class="form-control" disabled>' +
+                    '<option value="">select</option>' +
+                    '</select>'
+                );
             });
         }
     };
